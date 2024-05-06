19803dc8c6e43d824ed88c234dd2bdfb14bdc45c
Pablo Bengoechea Navarro <pbengoec@c3r10s5.42malaga.com>
2024-05-06 15:50:57 +0200

    Waitin 10 seconds for database to be created

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 service mariadb start
+
 mariadb -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+
 mariadb -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';"
+
 mariadb -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
+
 mariadb -u root -e "FLUSH PRIVILEGES;"
+
 mariadb -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_ROOT_PWD');"
+
 mariadb -u root -e "FLUSH PRIVILEGES;"
+
 mariadb -u root -p$DB_ROOT_PWD -e "GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';"
+
 mariadb -u root -p$DB_ROOT_PWD -e "FLUSH PRIVILEGES;"
+
 service mariadb stop
