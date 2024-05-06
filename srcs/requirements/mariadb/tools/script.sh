0d67e8dcb81eede41c4cc11a08215541828e6912
Pablo Bengoechea Navarro <pbengoec@c3r10s5.42malaga.com>
2024-05-06 18:31:37 +0200

    change dockerfile mysql

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 service mariadb start
-mariadb -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-mariadb -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';"
-mariadb -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
-mariadb -u root -e "FLUSH PRIVILEGES;"
-mariadb -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_ROOT_PWD');"
-mariadb -u root -e "FLUSH PRIVILEGES;"
-mariadb -u root -p$DB_ROOT_PWD -e "GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';"
-mariadb -u root -p$DB_ROOT_PWD -e "FLUSH PRIVILEGES;"
-mariadb -u root -p$DB_ROOT_PWD -e "shutdown;"
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD' ;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+
+mariadb < db1.sql
+
+kill $(cat /var/run/mysqld/mysqld.pid)
 
 mariadbd
